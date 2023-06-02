83c506b542323b317e41b7e9f47006d8da35480c
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2023-06-02 16:55:02 +0600

    eslint setup

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,15 @@ import config from "./config/index";
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
+    // eslint-disable-next-line no-console
     console.log("👌 Database is connected successfully");
 
     app.listen(config.port, () => {
+      // eslint-disable-next-line no-console
       console.log(`Application listening on port ${config.port}`);
     });
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.log("👎 Failed to connect database");
   }
 }
